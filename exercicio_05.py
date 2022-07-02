21c0e2f01c461c99558ed97f50e7c2abc5df7583
Juliana Batista <108022439+JulianaBatista0807@users.noreply.github.com>
2022-07-02 13:57:21 -0300

    Add files via upload

---
+++ exercicio_05.py
@@ -0,0 +1,8 @@
+n = int(input('Digite um numero: '))
+sucessor = n + 1
+antecessor = n - 1
+print("O número {} possui como sucessor o número {} e como antecessor o número {}".format(
+    n, sucessor, antecessor))
+
+# se não for usar as variaveis depois
+#print("O número {} possui como sucessor o número {} e como antecessor o número {}".format(n, (n + 1), (n - 1)))
---
+++ exercicio_06.py
@@ -0,0 +1,7 @@
+n = int(input('Digite um numero: '))
+dobro = n * 2
+triplo = n * 3
+raiz_quadrada = pow(n, (1/2))  # ou usar -> n ** (1/2)
+print("O dobro de {} é igual a {}. ".format(n, dobro,))
+print("O triplo de {} é igual a {}. ".format(n, triplo,))
+print("A raiz quadrada de {} é igual a {:.2f}. ".format(n, raiz_quadrada,))
