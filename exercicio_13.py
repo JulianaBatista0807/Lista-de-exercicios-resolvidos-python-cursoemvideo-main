a2b726f217eebc28611fb4cf05a70ecb7aa76ceb
Juliana Batista <108022439+JulianaBatista0807@users.noreply.github.com>
2022-09-24 20:06:26 -0300

    Create exercicio_13.py

---
+++ exercicio_13.py
@@ -0,0 +1,5 @@
+salario = float(input("Digite o seu salario: "))
+aumento = (salario * 15/100)
+novo_salario = salario + aumento
+
+print("Seu salario com o aumento de 15% é de: {}".format(novo_salario))
